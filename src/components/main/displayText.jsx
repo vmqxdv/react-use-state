da0a0eb6e6ff3d13f7febd3ca627ec6da76edeee
vmqxdv <vquintilio@gmail.com>
2025-04-08 14:44:02 +0200

    feat: Aggiunta funzione per mostrare testo

--- src/components/main/displayText.jsx
+++ src/components/main/displayText.jsx
@@ -1,6 +1,30 @@
-export default function displayText() {
+import { useState } from 'react';
+
+export default function displayText(props) {
+  const { data } = props;
+  const [text, changetext] = useState('Nessun linguaggio selezionato');
+
+  const formatText = (element) => {
+    return (
+      <>
+        <h2>{element.title}</h2>{element.description}
+      </>
+    )
+  };
+
+  const buttons = data.map(element => 
+    <button onClick={() => changetext(formatText(element))}>{element.title}</button>
+  );
+
   return (
-   <>
-   </> 
+    <>
+      <div className="buttons-container">
+        {buttons}
+      </div>
+
+      <div className="display-text">
+        <p>{text}</p>
+      </div>
+    </>
   )
 };
