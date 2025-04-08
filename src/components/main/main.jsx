f75f689ea774f92dfada0a0088d0199ad62f0bb3
vmqxdv <vquintilio@gmail.com>
2025-04-08 14:44:13 +0200

    feat: Importato DisplayText

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,11 @@
-export default function main() {
+import DisplayText from "./displayText"
+
+export default function main(props) {
+
+
   return (
    <>
+    <DisplayText data={props.data}/>
    </> 
   )
 };
